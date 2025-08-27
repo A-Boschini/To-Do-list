0b6069e438bad0815e123e2425e6c637051e5576
Alberto <albertoboschinicr@gmail.com>
2025-08-26 18:54:46 -0600

    JS 2

--- scripts.js
+++ scripts.js
@@ -8,4 +8,48 @@ function addTodo(text, completed =false) {
     if(completed) {
         li.classList.add("completed");
     }
+    li.innerHTML = ‛
+      <input type="checkbox" ${completed ? "checked" : ""} />
+      <label>$(text)</label>
+      <div class="todo-actions">
+          <div class="icon edit-icon">
+              <img src="edit.svg" alt="Edit" />
+          </div>
+          <div class="icon delete-icon">
+              <img src="delete.svg" alt="Delete" />
+          </div>
+    </div>
+‛;  
+todoList.appendChild(li); 
 }
+
+function handleAddButtonClick(){
+    const text = newTodoInput.ariaValueMax.trim();
+    if(text !=){
+        addTodo(text);
+        newTodoInput.value = "";
+    }
+}
+
+function toggleTodoCompleted(target) {
+    const item = target.closes(".todo-item");
+    item.classList.toggle("completed");
+}
+
+funtion deleteTodoItem(target){
+    const item = target.closest(".todo-item");
+    item.remove();
+}
+
+function handleTodoListClick(e) {
+   const target = e.target;
+   if(target.type === "checkbox") {
+    toggleTodoCompleted(target);
+   } 
+   if(target.closest(".delete-icon")) {
+    deleteTodoItem(target);
+   }
+}
+
+addButton.addEventListener("click", handleAddButtonClick);
+todoList.addEventListener("click", handleTodoListClick);
